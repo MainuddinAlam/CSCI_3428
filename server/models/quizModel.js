010395f604ffa73baa8194aad58d575648a84612
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-11-13 19:58:20 -0400

    updated code for quiz

--- server/models/quizModel.js
+++ server/models/quizModel.js
@@ -1,18 +1,24 @@
+/**
+ * Schema for the quiz page
+ *
+ * Idea of using Schemas: Altaf
+ * Author for the quiz schema: Mainuddin Alam Irteja (A00446752)
+ */
 const mongoose = require("mongoose");
 
 const quizSchema = new mongoose.Schema({
-    question: {
-        type: String,
-        required: true,
-    },
-    options: {
-        type: [String],
-        required: true,
-    },
-    answer: {
-        type: String,
-        required: true,
-    },
+  question: {
+    type: String,
+    required: true,
+  },
+  options: {
+    type: [String],
+    required: true,
+  },
+  answer: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model("Quiz", quizSchema);
+module.exports = mongoose.model("quiz", quizSchema);
